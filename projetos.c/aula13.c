fc2eae0fec5c549bcc6ebd3d2fa035403e09cc55
Davidev-pyt <serranojoseivan4@gmail.com>
2026-01-30 11:32:41 -0300

    Add files via upload

---
+++ projetos.c/aula13.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+//Programa de cálculo de média
+int main() {
+
+    float nota1, nota2, nota3;
+    float media;
+
+    printf("*** Programa de cálculo de média ***\n");
+
+    printf("Digite a primeira nota: \n");
+    scanf("%f", &nota1);
+
+    printf("Digite a segunda nota: \n");
+    scanf("%f", &nota2);
+
+    printf("Digite a terceira nota: \n");
+    scanf("%f", &nota3);
+
+     media = (nota1 + nota2 + nota3) / 3;
+
+     printf("A media é: %.1f\n", media);
+       return 0 ;
+
+
+
+}
